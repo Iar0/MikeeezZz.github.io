0c98089d34e8e4cd3e56bfb19e58ec7c35422525
Michele <patraxrock@gmail.com>
2014-10-17 19:53:33 +0200

    Modal

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -29,7 +29,27 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
    // animate
    $('html, body').animate({
        scrollTop: $(this.hash).offset().top -50
-     }, 300, function(){
+     }, 400, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 400, function(){
 
        // when done, add hash to url
        // (default click behaviour)
